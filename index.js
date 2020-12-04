5fcb33593cb18a8662451ce798bf2980b8ad2476
Odette Bahome <odettebahome@gmail.com>
2020-12-04 11:48:38 +0000

    iss fly over times

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ fetchMyIP((error, ip) => {
 });*/
 
 // The code below is temporary and can be commented out.
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 
+/*const {fetchMyIP} = require('./iss');
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -20,13 +20,29 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
   //console.log('Coords:', coords);
-});
+}); */
 
-fetchCoordsByIP((error, coords) => {
+// The code below is temporary and can be commented out.
+//const { fetchCoordsByIP } = require('./iss');
+
+/*fetchCoordsByIP('162.245.144.188', (error, coords) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('Coords:', coords);
+  console.log('It worked! Returned Coords:' , coords);
+});*/
+
+const {fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
