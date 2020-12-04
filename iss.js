5208778c2503fc900af910ca4edd886ef889c900
Odette Bahome <odettebahome@gmail.com>
2020-12-04 01:58:32 +0000

    fetch coords by IP

--- iss.js
+++ iss.js
@@ -6,7 +6,7 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const https = require('https');
+/*const https = require('https');
 const searchUrl = 'https://api.ipify.org?format=json';
 
 const fetchMyIP = function(callback) { 
@@ -22,6 +22,44 @@ https.get(searchUrl, (resp) => {
       });
 }
  });
+};*/
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(Callback) {
+  request('https://ipvigilante.com/8.8.8.8', (error, response, body) => {
+    if (error) return callback(error, null);
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+    callback(Error(msg), null);
+      return;
+    }
+    const longitude = JSON.parse(body).data.longitude;
+    const latitude = JSON.parse(body).data.latitude;
+    const coords = {longitude, latitude};
+    callback(null, coords);
+  });
+}
+module.exports = { fetchMyIP, fetchCoordsByIP};
