e53e2f5d8fa8a1df66468c67c34c2a0757b489c3
Odette Bahome <odettebahome@gmail.com>
2020-12-04 01:57:43 +0000

    fetch coords

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+/*const { fetchMyIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,4 +6,27 @@ fetchMyIP((error, ip) => {
     return;
   }
   
+});*/
+
+// The code below is temporary and can be commented out.
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+  //console.log('Coords:', coords);
+});
+
+fetchCoordsByIP((error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('Coords:', coords);
 });
